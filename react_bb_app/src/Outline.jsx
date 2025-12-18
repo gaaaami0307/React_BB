0a55d707a1df4c84e4e4ccc1d54c45fd63555480
Gami <s1320016@u-aizu.ac.jp>
2025-12-18 18:08:22 +0900

    add: Outline jsx

---
+++ react_bb_app/src/Outline.jsx
@@ -0,0 +1,25 @@
+
+function Outline() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* ヘッダー */}
+      <header className="bg-[#edfffd] h-16">
+        
+      </header>
+      
+      <div className="flex flex-1">
+        {/* サイドバー */}
+        <aside className="bg-white w-64">
+          
+        </aside>
+      
+        {/* メインコンテンツ */}
+        <main className="flex-1 bg-[#fafafa] p-4">
+          テスト
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Outline
--- react_bb_app/src/main.jsx
+++ react_bb_app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Outline from './Outline.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Outline />
   </StrictMode>,
 )
