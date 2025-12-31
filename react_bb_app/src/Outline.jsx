0cb514c8b51d81bb443fd538aff61a1a7e2b53ac
Gami <s1320016@u-aizu.ac.jp>
2025-12-31 20:25:48 +0900

    make header and sidebar

--- react_bb_app/src/Outline.jsx
+++ react_bb_app/src/Outline.jsx
@@ -3,13 +3,24 @@ function Outline() {
   return (
     <div className="min-h-screen flex flex-col">
       {/* ヘッダー */}
-      <header className="bg-[#edfffd] h-16">
-        
-      </header>
+      <header className="bg-[#edfffd] h-24 min-w-screen relative overflow-hidden">
+      {/* 斜めの装飾 */}
+      <div className="absolute right-0 top-0 w-1/2 h-full bg-[#7fffd4] 
+          transform skew-x-[-20deg] translate-x-20"></div>
+    </header>
       
       <div className="flex flex-1">
         {/* サイドバー */}
-        <aside className="bg-white w-64">
+        <aside className="bg-white w-24">
+
+          <div className="w-12 bg-[#00aa00] min-h-screen">
+
+          </div>
+
+          {/* 影枠 */}
+          <div className="w-24 bg-[#aa0000] min-h-screen">
+
+          </div>
           
         </aside>
       
