1db17369e39a373824291f2d7cb455797e577724
Gami <s1320016@u-aizu.ac.jp>
2025-12-31 22:29:32 +0900

    fix design

--- react_bb_app/src/Outline.jsx
+++ react_bb_app/src/Outline.jsx
@@ -6,29 +6,20 @@ function Outline() {
       <header className="bg-[#edfffd] h-24 min-w-screen relative overflow-hidden">
       {/* 斜めの装飾 */}
       <div className="absolute right-0 top-0 w-1/2 h-full bg-[#7fffd4] 
-          transform skew-x-[-20deg] translate-x-20 z-10"></div>
+          transform skew-x-[-20deg] translate-x-20 z-100"></div>
       <div className="absolute right-0 top-0 w-1/2 h-full bg-[#5fdfb4] 
-        transform skew-x-[-20deg] translate-x-17 z-0"></div>
+        transform skew-x-[-20deg] translate-x-17 z-90"></div>
 
     </header>
       
       <div className="flex flex-1">
         {/* サイドバー */}
-        <aside className="bg-white w-24">
-
-          <div className="w-12 bg-[#00aa00] min-h-screen">
-
-          </div>
-
-          {/* 影枠 */}
-          <div className="w-24 bg-[#aa0000] min-h-screen">
-
-          </div>
-          
+        <aside className="bg-[#ffffff] w-24 max-h-screen relative z-100 overflow-hidden">
+          <div className="absolute bg-[#eeeeee] left-23 w-2 h-full z-110"></div>
         </aside>
       
         {/* メインコンテンツ */}
-        <main className="flex-1 bg-[#fafafa] p-4">
+        <main className="flex-1 bg-[#f8f8f8] p-4">
           テスト
         </main>
       </div>
